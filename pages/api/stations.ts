c95d9a432d0e7c384b7840a5c3a006d4c3cc07ca
jotham <jotham@student.ethz.ch>
2024-11-27 17:03:23 +0100

    stations api

--- pages/api/stations.ts
+++ pages/api/stations.ts
@@ -1,19 +1,39 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import fs from "node:fs/promises"
 
 type Station = {
+	id: number,
 	name: string,
+	type: string,
+	lines: string,
 	x: number,
 	y: number
 }
 
-type ResponseData = {
-	stations: Station[]
-}
+type ResponseData = Station[]
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
-	let stations: Station[] = [
-		{name: "test1", x: 0, y: 0},
-		{name: "test2", x: 10, y: 20},
-	]
-	res.status(200).json({stations})
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+	let csv = await fs.readFile("data/stations.csv")
+
+	let lines = csv.toString().split("\n")
+	let stationsRaw = []
+	for (let line of lines) {
+		let s = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)
+		stationsRaw.push(s)
+	}
+	stationsRaw.shift()
+
+	let stations: Station[] = stationsRaw.map((s) => {
+		return {
+			id: Number(s[0]),
+			name: s[4]?.replace(/['"]+/g, ""),
+			type: s[6]?.replace(/['"]+/g, ""),
+			lines: s[13]?.replace(/['"]+/g, ""),
+			x: Number(s[14]),
+			y: Number(s[15])
+		}
+	})
+	stations = stations.filter((s) => s.type && s.type.toLowerCase().includes("tram"))
+
+	res.status(200).json(stations)
 }
