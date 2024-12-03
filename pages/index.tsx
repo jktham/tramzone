5265be01e6c8128e2eaff175a1ec92b32cbd8fd8
lazygreatpanda <lazygreatpanda@gmail.com>
2024-12-03 16:22:06 +0100

    Balls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "ol/ol.css";
-import Map from "ol/Map";
-import View from "ol/View";
-import TileLayer from "ol/layer/Tile";
+import Map from "ol/Map.js";
+import View from "ol/View.js";
+import TileLayer from "ol/layer/Tile.js";
 import VectorLayer from "ol/layer/Vector.js";
 import StadiaMaps from "ol/source/StadiaMaps.js";
-import OSM from "ol/source/OSM";
+import OSM from "ol/source/OSM.js";
 import * as OlProj from "ol/proj";
 import useSWR from "swr";
 import VectorSource from "ol/source/Vector";
 import GeoJSON from "ol/format/GeoJSON";
+import Style from "ol/style/Style.js";
+import { Circle, Stroke, Fill } from "ol/style.js";
+import { features } from "process";
 
 export default function Home() {
-  const { data, error, isLoading } = useSWR("api/stations", (url) =>
+  const { data, isLoading } = useSWR("api/stations", (url) =>
     fetch(url).then((res) => res.json())
   );
-  const getSationData = async (data) => {
+
+  const getSationData: Object = (data) => {
     let geoJson = { type: "FeatureCollection", features: [] };
     for (let point in data) {
       let coordinates = point.coords;
       let properties = point.name;
-      // delete properties.coords;
       let feature = {
         type: "Feature",
         geometry: { type: "Point", coordinates: coordinates },
@@ -31,14 +34,29 @@ export default function Home() {
     return geoJson;
   };
 
+  let geoJSON: Object = { type: "FeatureCollection", features: [] };
+  console.log(geoJSON);
+  if (!isLoading) {
+    console.log(data, isLoading);
+    geoJSON = getSationData(data);
+  }
+
   useEffect(() => {
-    const geoJSON = getSationData(data);
+    let olView = new View({
+      center: OlProj.fromLonLat([8.5417, 47.3769]),
+      zoom: 15,
+    });
+
+    console.log(geoJSON);
+    const features = new GeoJSON().readFeatures(geoJSON, {
+      featureProjection: olView.getProjection(),
+    });
 
     const stationLayer = new VectorLayer({
       source: new VectorSource({
-        features: data,
-        format: new GeoJSON(),
+        features: features,
       }),
+      visible: true,
     });
 
     const map = new Map({
@@ -50,21 +68,24 @@ export default function Home() {
             retina: true,
           }),
         }),
-        stationLayer,
+        //stationLayer,
       ],
     });
 
-    map.setView(
-      new View({
-        center: OlProj.fromLonLat([8.5417, 47.3769]),
-        zoom: 15,
-      })
-    );
+    map.setView(olView);
 
     return () => {
       map.setTarget(null);
     };
   }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <div>Is Loading...</div>
+      </>
+    );
+  }
 
   return (
     <>
