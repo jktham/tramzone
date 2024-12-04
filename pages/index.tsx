2283b77f1a9e9532de8322a0ebf21df54429d940
lazygreatpanda <lazygreatpanda@gmail.com>
2024-12-04 10:46:23 +0100

    Added drawing functionality from Jonas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,105 +1,26 @@
-import React, { useEffect, useState } from "react";
-import "ol/ol.css";
-import Map from "ol/Map.js";
-import View from "ol/View.js";
-import TileLayer from "ol/layer/Tile.js";
-import VectorLayer from "ol/layer/Vector.js";
-import StadiaMaps from "ol/source/StadiaMaps.js";
-import OSM from "ol/source/OSM.js";
-import * as OlProj from "ol/proj";
 import useSWR from "swr";
-import VectorSource from "ol/source/Vector";
-import GeoJSON from "ol/format/GeoJSON";
-import Style from "ol/style/Style.js";
-import { Circle, Stroke, Fill } from "ol/style.js";
-import { features } from "process";
-import { convertLV95toWGS84 } from "../utils/mapUtils";
 import TramMap from "../components/tramMap";
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function Home() {
-  const { data, isLoading } = useSWR("api/stations", (url) =>
-    fetch(url).then((res) => res.json())
-  );
+  const {
+    data: lineData,
+    error: linesError,
+    isLoading: linesLoading,
+  } = useSWR("/api/lines", fetcher);
+  const {
+    data: stationData,
+    error: stationsError,
+    isLoading: stationsLoading,
+  } = useSWR("/api/stations", fetcher);
+  const {
+    data: tramData,
+    error: tramsError,
+    isLoading: tramsLoading,
+  } = useSWR("/api/trams?active=true", fetcher);
 
-  const getSationData = (data) => {
-    let geoJson = { type: "FeatureCollection", features: [] };
-    for (let point of data) {
-      let coordinates = convertLV95toWGS84(point.coords);
-      let properties = point.name;
-      let feature = {
-        type: "Feature",
-        geometry: { type: "Point", coordinates: coordinates },
-        properties: properties,
-      };
-      geoJson.features.push(feature);
-    }
-    return geoJson;
-  };
-
-  let geoJSON = { type: "FeatureCollection", features: [] };
-  console.log(geoJSON);
-  if (!isLoading) {
-    console.log(data, isLoading);
-    geoJSON = getSationData(data);
-  }
-
-  // Style for the Stations Layer
-  const fill = new Fill({
-    color: "rgba(0,0,0,1)",
-  });
-
-  const stroke = new Stroke({
-    color: "#3399CC",
-    width: 0,
-  });
-
-  useEffect(() => {
-    let olView = new View({
-      center: OlProj.fromLonLat([8.5417, 47.3769]),
-      zoom: 15,
-    });
-
-    console.log(geoJSON);
-    const features = new GeoJSON().readFeatures(geoJSON, {
-      featureProjection: olView.getProjection(),
-    });
-
-    const stationLayer = new VectorLayer({
-      source: new VectorSource({
-        features: features,
-      }),
-      visible: true,
-      style: new Style({
-        image: new Circle({
-          fill: fill,
-          //stroke: stroke,
-          radius: 5,
-        }),
-        fill: fill,
-        //stroke: stroke,
-      }),
-    });
-
-    const map = new Map({
-      target: "map",
-      layers: [
-        new TileLayer({
-          source: new StadiaMaps({
-            layer: "alidade_smooth",
-            retina: true,
-          }),
-        }),
-        stationLayer,
-      ],
-      view: olView,
-    });
-
-    return () => {
-      map.setTarget(null);
-    };
-  }, [geoJSON]);
-
-  if (isLoading) {
+  if (linesLoading || stationsLoading || tramsLoading) {
     return (
       <>
         <div>Is Loading...</div>
--- utils/parseData.ts
+++ utils/parseData.ts
@@ -1,30 +1,53 @@
-import {promises as fs, createReadStream, createWriteStream, write} from "node:fs"
-import stream from "node:stream"
-import unzipper from "unzipper"
-import "util/types"
-let shapefile = require("shapefile")
-import csv from "csv-parser"
-import readline from "node:readline"
+import {
+  promises as fs,
+  createReadStream,
+  createWriteStream,
+  write,
+} from "node:fs";
+import stream from "node:stream";
+import unzipper from "unzipper";
+import "util/types";
+let shapefile = require("shapefile");
+import csv from "csv-parser";
+import readline from "node:readline";
 
 function getDate() {
-	// new gtfs data every monday and thursday (todo: static at 10:00, rt at 15:00)
-	let monday = new Date()
-	monday.setDate(monday.getDate() - (monday.getDay() + 6) % 7)
-	let thursday = new Date()
-	thursday.setDate(thursday.getDate() - (thursday.getDay() + 3) % 7)
-	let date = new Date(Math.max(monday.getTime(), thursday.getTime()))
-    	.toISOString()
-    	.substring(0, 10);
-  	return date;
+  // new gtfs data every monday and thursday (static at 10:00, rt at 15:00)
+  let updateTime = new Date();
+  updateTime.setHours(15, 30, 0, 0);
+
+  let monday = new Date();
+  monday.setDate(monday.getDate() - ((monday.getDay() + 6) % 7));
+  let thursday = new Date();
+  thursday.setDate(thursday.getDate() - ((thursday.getDay() + 3) % 7));
+
+  if (
+    monday.getDate() == updateTime.getDate() &&
+    monday.getTime() < updateTime.getTime()
+  ) {
+    monday.setDate(monday.getDate() - 7);
+  }
+  if (
+    thursday.getDate() == updateTime.getDate() &&
+    thursday.getTime() < updateTime.getTime()
+  ) {
+    thursday.setDate(thursday.getDate() - 7);
+  }
+
+  let date = new Date(Math.max(monday.getTime(), thursday.getTime()))
+    .toISOString()
+    .substring(0, 10);
+  return date;
 }
 
-function getISO(time: string) { // time only
-	let h = Number(time.split(":")[0])
-	let m = Number(time.split(":")[1])
-	let s = Number(time.split(":")[2])
-	let d = new Date(0)
-	d.setHours(h, m, s, 0)
-	return d.getTime()
+function getISO(time: string) {
+  // time only
+  let h = Number(time.split(":")[0]);
+  let m = Number(time.split(":")[1]);
+  let s = Number(time.split(":")[2]);
+  let d = new Date(0);
+  d.setHours(h, m, s, 0);
+  return d.getTime();
 }
 
 async function getGtfs() {
@@ -77,16 +100,16 @@ async function parseGtfs() {
   }
   routesRaw.shift();
 
-	let routes: Route[] = routesRaw.map((r) => {
-		return {
-			route_id: r[0]?.replace(/['"]+/g, ""),
-			name: r[2]?.replace(/['"]+/g, ""),
-			type: r[5]?.replace(/['"]+/g, ""),
-			agency: r[1]?.replace(/['"]+/g, "")
-		}
-	})
-	routes = routes.filter((s) => s.type == "900" && s.agency == "3849") // tram && VBZ
-	await fs.writeFile("data/parsed/routes.json", JSON.stringify(routes))
+  let routes: Route[] = routesRaw.map((r) => {
+    return {
+      route_id: r[0]?.replace(/['"]+/g, ""),
+      name: r[2]?.replace(/['"]+/g, ""),
+      type: r[5]?.replace(/['"]+/g, ""),
+      agency: r[1]?.replace(/['"]+/g, ""),
+    };
+  });
+  routes = routes.filter((s) => s.type == "900" && s.agency == "3849"); // tram && VBZ
+  await fs.writeFile("data/parsed/routes.json", JSON.stringify(routes));
 
   // trips
   console.log("parsing gtfs trips");
@@ -98,107 +121,110 @@ async function parseGtfs() {
   }
   tripsRaw.shift();
 
-	let trips: Trip[] = tripsRaw.map((t) => {
-		return {
-			trip_id: t[2]?.replace(/['"]+/g, ""),
-			route_id: t[0]?.replace(/['"]+/g, ""),
-			service_id: t[1]?.replace(/['"]+/g, ""),
-			headsign: t[3]?.replace(/['"]+/g, ""),
-			name: t[4]?.replace(/['"]+/g, ""),
-			direction: Number(t[5]?.replace(/['"]+/g, ""))
-		}
-	})
-	let routeIds = new Set(routes.map((r) => r.route_id))
-	trips = trips.filter((t) => routeIds.has(t.route_id))
-	await fs.writeFile("data/parsed/trips.json", JSON.stringify(trips))
+  let trips: Trip[] = tripsRaw.map((t) => {
+    return {
+      trip_id: t[2]?.replace(/['"]+/g, ""),
+      route_id: t[0]?.replace(/['"]+/g, ""),
+      service_id: t[1]?.replace(/['"]+/g, ""),
+      headsign: t[3]?.replace(/['"]+/g, ""),
+      name: t[4]?.replace(/['"]+/g, ""),
+      direction: Number(t[5]?.replace(/['"]+/g, "")),
+    };
+  });
+  let routeIds = new Set(routes.map((r) => r.route_id));
+  trips = trips.filter((t) => routeIds.has(t.route_id));
+  await fs.writeFile("data/parsed/trips.json", JSON.stringify(trips));
 
-	// stop_times
-	console.log("filtering gtfs stop times")
+  // stop_times
+  console.log("filtering gtfs stop times");
 
-	let first = true
-	let tripIds = new Set(trips.map((t) => t.trip_id))
-	let filteredWriteStream = createWriteStream("data/parsed/stopTimesFiltered.csv")
-	await new Promise<void>((resolve) => {
-		let rd = readline.createInterface({
-			input: createReadStream(`data/gtfs/${date}/stop_times.txt`), // big ass file
-			terminal: false
-		})
-	
-		rd.on('line', function(line) {
-			let trip_id = line.split(",")[0].replace(/['"]+/g, "")
-			if (first || tripIds.has(trip_id)) {
-				filteredWriteStream.write(line + "\n");
-			}
-			first = false
-		})
-		
-		rd.on('close', function() {
-			resolve()
-		})
-	})
+  let first = true;
+  let tripIds = new Set(trips.map((t) => t.trip_id));
+  let filteredWriteStream = createWriteStream(
+    "data/parsed/stopTimesFiltered.csv"
+  );
+  await new Promise<void>((resolve) => {
+    let rd = readline.createInterface({
+      input: createReadStream(`data/gtfs/${date}/stop_times.txt`), // big ass file
+      terminal: false,
+    });
 
-	console.log("parsing gtfs stop times")
+    rd.on("line", function (line) {
+      let trip_id = line.split(",")[0].replace(/['"]+/g, "");
+      if (first || tripIds.has(trip_id)) {
+        filteredWriteStream.write(line + "\n");
+      }
+      first = false;
+    });
 
-	let readStream = createReadStream("data/parsed/stopTimesFiltered.csv")
-	let writeStream = createWriteStream("data/parsed/stopTimes.json")
+    rd.on("close", function () {
+      resolve();
+    });
+  });
 
-	writeStream.on("drain", () => {
-		readStream.resume()
-	})
-	writeStream.write("[")
+  console.log("parsing gtfs stop times");
 
-	let sep = ""
-	await new Promise<void>((resolve) => {
-		readStream
-			.pipe(csv())
-			.on("data", (row) => {
-				let station: StopTime = {
-					trip_id: String(Object.values(row)[0])?.replace(/['"]+/g, ""), // for some reason using the key doesnt work
-					arrival: getISO(row["arrival_time"]?.replace(/['"]+/g, "")),
-					departure: getISO(row["departure_time"]?.replace(/['"]+/g, "")),
-					stop_id: row["stop_id"]?.replace(/['"]+/g, ""),
-					stop_sequence: Number(row["stop_sequence"]?.replace(/['"]+/g, ""))
-				}
-				if (!writeStream.write(sep + JSON.stringify(station))) {
-					readStream.pause()
-				}
-				if (sep == "") {
-					sep = ","
-				}
-			})
-			.on("end", () => {
-				resolve()
-			})
-		}
-	)
-	writeStream.write("]")
+  let readStream = createReadStream("data/parsed/stopTimesFiltered.csv");
+  let writeStream = createWriteStream("data/parsed/stopTimes.json");
 
-	// calendar
-	console.log("parsing services")
+  writeStream.on("drain", () => {
+    readStream.resume();
+  });
+  writeStream.write("[");
 
-	let servicesCsv = await fs.readFile(`data/gtfs/${date}/calendar.txt`)
-	let servicesRaw = []
-	for (let line of servicesCsv.toString().split("\r\n")) {
-		let s = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)
-		servicesRaw.push(s)
-	}
-	servicesRaw.shift()
+  let sep = "";
+  await new Promise<void>((resolve) => {
+    readStream
+      .pipe(csv())
+      .on("data", (row) => {
+        let station: StopTime = {
+          trip_id: String(Object.values(row)[0])?.replace(/['"]+/g, ""), // for some reason using the key doesnt work
+          arrival: getISO(row["arrival_time"]?.replace(/['"]+/g, "")),
+          departure: getISO(row["departure_time"]?.replace(/['"]+/g, "")),
+          stop_id: row["stop_id"]?.replace(/['"]+/g, ""),
+          stop_sequence: Number(row["stop_sequence"]?.replace(/['"]+/g, "")),
+        };
+        if (!writeStream.write(sep + JSON.stringify(station))) {
+          readStream.pause();
+        }
+        if (sep == "") {
+          sep = ",";
+        }
+      })
+      .on("end", () => {
+        resolve();
+      });
+  });
+  writeStream.write("]");
 
-	// todo: consider calendar_dates
-	let services: Service[] = servicesRaw.map((s) => {
-		return {
-			service_id: s[0]?.replace(/['"]+/g, ""),
-			days: [Number(s[1]?.replace(/['"]+/g, "")), 
-				Number(s[2]?.replace(/['"]+/g, "")), 
-				Number(s[3]?.replace(/['"]+/g, "")), 
-				Number(s[4]?.replace(/['"]+/g, "")), 
-				Number(s[5]?.replace(/['"]+/g, "")), 
-				Number(s[6]?.replace(/['"]+/g, "")), 
-				Number(s[7]?.replace(/['"]+/g, ""))
-			]
-		}
-	})
-	await fs.writeFile("data/parsed/services.json", JSON.stringify(services))
+  // calendar
+  console.log("parsing services");
+
+  let servicesCsv = await fs.readFile(`data/gtfs/${date}/calendar.txt`);
+  let servicesRaw = [];
+  for (let line of servicesCsv.toString().split("\r\n")) {
+    let s = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+    servicesRaw.push(s);
+  }
+  servicesRaw.shift();
+
+  // todo: consider calendar_dates
+  let services: Service[] = servicesRaw.map((s) => {
+    return {
+      service_id: s[0]?.replace(/['"]+/g, ""),
+      days: [
+        Number(s[1]?.replace(/['"]+/g, "")),
+        Number(s[2]?.replace(/['"]+/g, "")),
+        Number(s[3]?.replace(/['"]+/g, "")),
+        Number(s[4]?.replace(/['"]+/g, "")),
+        Number(s[5]?.replace(/['"]+/g, "")),
+        Number(s[6]?.replace(/['"]+/g, "")),
+        Number(s[7]?.replace(/['"]+/g, "")),
+      ],
+    };
+  });
+  await fs.writeFile("data/parsed/services.json", JSON.stringify(services));
+}
 
 async function parseStations() {
   console.log("parsing dataset stations");
@@ -232,6 +258,9 @@ async function parseStations() {
 async function parseLines() {
   console.log("parsing dataset lines");
 
+  let lineColors = JSON.parse(
+    (await fs.readFile(`data/datasets/lineColors.json`)).toString()
+  );
   let geojson = await shapefile.read(
     "data/datasets/lines.shp",
     "data/datasets/lines.dbf"
@@ -246,41 +275,6 @@ async function parseLines() {
         sequence: feature.properties["SEQUENZNR"],
         geometry: feature.geometry,
       };
-	let lineColors = JSON.parse((await fs.readFile(`data/datasets/lineColors.json`)).toString())
-	let geojson = await shapefile.read("data/datasets/lines.shp", "data/datasets/lines.dbf")
-	let lines: Line[] = []
-	for (let feature of geojson.features) {
-		if (feature.properties["BETRIEBS00"]?.includes("VBZ-Tram")) {
-			let segment: Segment = {
-				from: feature.properties["VONHALTEST"],
-				to: feature.properties["BISHALTEST"],
-				direction: feature.properties["RICHTUNG"],
-				sequence: feature.properties["SEQUENZNR"],
-				geometry: feature.geometry
-			}
-			
-			let found = lines.find((l) => l.id === feature.properties["LINIENSCHL"])
-			if (found) {
-				found.segments.push(segment)
-			} else {
-				let line: Line = {
-					id: feature.properties["LINIENSCHL"],
-					name: feature.properties["LINIENNUMM"],
-					color: lineColors.find((l) => l.name == feature.properties["LINIENNUMM"])?.color || "#ffffff",
-					start: feature.properties["ANFANGSHAL"],
-					end: feature.properties["ENDHALTEST"],
-					segments: [segment]
-				}
-				lines.push(line)
-			}
-		}
-	}
-	lines.find((l) => l.name == "10").start = "Zürich, Bahnhofplatz/HB"
-	lines.sort((a, b) => Number(a.name) - Number(b.name))
-	for (let line of lines) {
-		line.segments.sort((a, b) => a.sequence - b.sequence)
-		line.segments.sort((a, b) => a.direction - b.direction)
-	}
 
       let found = lines.find((l) => l.id === feature.properties["LINIENSCHL"]);
       if (found) {
@@ -289,6 +283,9 @@ async function parseLines() {
         let line: Line = {
           id: feature.properties["LINIENSCHL"],
           name: feature.properties["LINIENNUMM"],
+          color:
+            lineColors.find((l) => l.name == feature.properties["LINIENNUMM"])
+              ?.color || "#ffffff",
           start: feature.properties["ANFANGSHAL"],
           end: feature.properties["ENDHALTEST"],
           segments: [segment],
@@ -297,6 +294,7 @@ async function parseLines() {
       }
     }
   }
+  lines.find((l) => l.name == "10").start = "Zürich, Bahnhofplatz/HB";
   lines.sort((a, b) => Number(a.name) - Number(b.name));
   for (let line of lines) {
     line.segments.sort((a, b) => a.sequence - b.sequence);
@@ -309,61 +307,76 @@ async function parseLines() {
 async function generateTramTrips() {
   console.log("generating tramTrips");
 
-	let routes: Route[] = JSON.parse((await fs.readFile("data/parsed/routes.json")).toString())
-	let trips: Trip[] = JSON.parse((await fs.readFile("data/parsed/trips.json")).toString())
-	let stopTimes: StopTime[] = JSON.parse((await fs.readFile("data/parsed/stopTimes.json")).toString())
-	let services: Service[] = JSON.parse((await fs.readFile("data/parsed/services.json")).toString())
+  let routes: Route[] = JSON.parse(
+    (await fs.readFile("data/parsed/routes.json")).toString()
+  );
+  let trips: Trip[] = JSON.parse(
+    (await fs.readFile("data/parsed/trips.json")).toString()
+  );
+  let stopTimes: StopTime[] = JSON.parse(
+    (await fs.readFile("data/parsed/stopTimes.json")).toString()
+  );
+  let services: Service[] = JSON.parse(
+    (await fs.readFile("data/parsed/services.json")).toString()
+  );
 
-	let stopTimesMap: Map<string, StopTime[]> = new Map()
-	stopTimes.map((s) => {
-		if (stopTimesMap.has(s.trip_id)) {
-			stopTimesMap.set(s.trip_id, [s, ...stopTimesMap.get(s.trip_id)])
-		} else {
-			stopTimesMap.set(s.trip_id, [s])
-		}
-	})
-	stopTimesMap.forEach((v, k) => {
-		stopTimesMap.set(k, v.sort((a, b) => a.stop_sequence - b.stop_sequence))
-	})
+  let stopTimesMap: Map<string, StopTime[]> = new Map();
+  stopTimes.map((s) => {
+    if (stopTimesMap.has(s.trip_id)) {
+      stopTimesMap.set(s.trip_id, [s, ...stopTimesMap.get(s.trip_id)]);
+    } else {
+      stopTimesMap.set(s.trip_id, [s]);
+    }
+  });
+  stopTimesMap.forEach((v, k) => {
+    stopTimesMap.set(
+      k,
+      v.sort((a, b) => a.stop_sequence - b.stop_sequence)
+    );
+  });
 
-	let servicesMap: Map<string, number[]> = new Map()
-	services.map((s) => {
-		servicesMap.set(s.service_id, s.days)
-	})
+  let servicesMap: Map<string, number[]> = new Map();
+  services.map((s) => {
+    servicesMap.set(s.service_id, s.days);
+  });
 
-	// todo: aaaa
-	let tramTrips: TramTrip[] = trips.map((t) => {
-		let r: Route = routes.find((r) => r.route_id == t.route_id)
-		let s: StopTime[] = stopTimesMap.get(t.trip_id)
-		return {
-			trip_id: t.trip_id,
-			trip_name: t.name,
-			headsign: t.headsign,
-			direction: t.direction,
-			route_id: r.route_id,
-			route_name: r.name,
-			service_id: t.service_id,
-			service_days: servicesMap.get(t.service_id),
-			stops: s.map((s) => {
-				return {
-					stop_id: s.stop_id,
-					stop_sequence: s.stop_sequence,
-					arrival: s.arrival,
-					departure: s.departure
-				}
-			})
-		}
-	})
-	.sort((a, b) => a.direction - b.direction)
-	.sort((a, b) => a.trip_id.localeCompare(b.trip_id))
-	.sort((a, b) => Number(a.route_name) - Number(b.route_name))
+  // todo: aaaa
+  let tramTrips: TramTrip[] = trips
+    .map((t) => {
+      let r: Route = routes.find((r) => r.route_id == t.route_id);
+      let s: StopTime[] = stopTimesMap.get(t.trip_id);
+      return {
+        trip_id: t.trip_id,
+        trip_name: t.name,
+        headsign: t.headsign,
+        direction: t.direction,
+        route_id: r.route_id,
+        route_name: r.name,
+        service_id: t.service_id,
+        service_days: servicesMap.get(t.service_id),
+        stops: s.map((s) => {
+          return {
+            stop_id: s.stop_id,
+            stop_sequence: s.stop_sequence,
+            arrival: s.arrival,
+            departure: s.departure,
+          };
+        }),
+      };
+    })
+    .sort((a, b) => a.direction - b.direction)
+    .sort((a, b) => a.trip_id.localeCompare(b.trip_id))
+    .sort((a, b) => Number(a.route_name) - Number(b.route_name));
 
-	fs.writeFile("data/parsed/tramTrips.json", JSON.stringify(tramTrips))
+  fs.writeFile("data/parsed/tramTrips.json", JSON.stringify(tramTrips));
 
-	for (let i=0; i<7; i++) {
-		let tramTrips_day = tramTrips.filter((t) => t.service_days[i] == 1)
-		fs.writeFile(`data/parsed/tramTrips${i}.json`, JSON.stringify(tramTrips_day))
-	}
+  for (let i = 0; i < 7; i++) {
+    let tramTrips_day = tramTrips.filter((t) => t.service_days[i] == 1);
+    fs.writeFile(
+      `data/parsed/tramTrips${i}.json`,
+      JSON.stringify(tramTrips_day)
+    );
+  }
 }
 
 async function main() {
@@ -374,13 +387,13 @@ async function main() {
     await fs.mkdir("data/parsed/");
   }
 
-	await parseLines()
-	await parseStations()
-	await getGtfs()
-	await parseGtfs()
-	await generateTramTrips()
+  await parseLines();
+  await parseStations();
+  await getGtfs();
+  await parseGtfs();
+  await generateTramTrips();
 
-	console.log("done")
+  console.log("done");
 }
 
-main()
+main();
