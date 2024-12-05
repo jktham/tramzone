6c2ef0b806bd8ddcd1f5da6a81b48d739591aee4
srpnt3 <virushd93@gmail.com>
2024-12-05 16:13:09 +0100

    tram move now, but the way they move is very questionable

--- components/tramMap.tsx
+++ components/tramMap.tsx
@@ -15,184 +15,172 @@ import Style from "ol/style/Style.js";
 import {Circle, Stroke, Fill} from "ol/style.js";
 import "../utils/types";
 import {Layer} from "ol/layer";
+import ol from "ol/dist/ol";
 
 export default function TramMap({lineData, stationData, tramData}: { lineData: Line[]; stationData: Station[]; tramData: Tram[]; }) {
 
-  // FUNCTIONS TO PARSE THE DATA TODO: put in different file ////
-  const getStationData = (data: Station[]) => {
-    let geoJson = {type: "FeatureCollection", features: []};
-    for (let station of data) {
-      let feature = {
-        type: "Feature",
-        geometry: {type: "Point", coordinates: station.coords},
-        properties: {
-          name: station.name,
-        },
-      };
-      geoJson.features.push(feature);
-    }
-    return geoJson;
-  };
+	// FUNCTIONS TO PARSE THE DATA TODO: put in different file ////
+	const getStationData = (data: Station[]) => {
+		let geoJson = {type: "FeatureCollection", features: []};
+		for (let station of data) {
+			let feature = {
+				type: "Feature",
+				geometry: {type: "Point", coordinates: station.coords},
+				properties: {
+					name: station.name,
+				},
+			};
+			geoJson.features.push(feature);
+		}
+		return geoJson;
+	};
 
-  const getLineData = (data: Line[]) => {
-    let geoJSON = {type: "FeatureCollection", features: []};
-    for (let line of data) {
-      for (let segment of line.segments) {
-        let feature = {
-          type: "Feature",
-          geometry: segment.geometry,
-          properties: {
-            name: line.name,
-            color: line.color,
-          },
-        };
-        geoJSON.features.push(feature);
-      }
-    }
-    return geoJSON;
-  };
+	const getLineData = (data: Line[]) => {
+		let geoJSON = {type: "FeatureCollection", features: []};
+		for (let line of data) {
+			for (let segment of line.segments) {
+				let feature = {
+					type: "Feature",
+					geometry: segment.geometry,
+					properties: {
+						name: line.name,
+						color: line.color,
+					},
+				};
+				geoJSON.features.push(feature);
+			}
+		}
+		return geoJSON;
+	};
 
-  const getTramData = (data: Tram[]) => {
-    let geoJSON = {type: "FeatureCollection", features: []};
-    for (let tram of data) {
-      let feature = {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: getTramLocation(tram, lineData),
-        },
-        properties: {
-          name: tram.route_name,
-          color: lineData.find((l) => l.name == tram.route_name)?.color,
-        },
-      };
-      geoJSON.features.push(feature);
-    }
-    return geoJSON;
-  };
-  // TODO: up until here ////
+	const getTramData = (data: Tram[]) => {
+		let geoJSON = {type: "FeatureCollection", features: []};
+		for (let tram of data) {
+			let feature = {
+				type: "Feature",
+				geometry: {
+					type: "Point",
+					coordinates: getTramLocation(tram, lineData),
+				},
+				properties: {
+					name: tram.route_name,
+					color: lineData.find((l) => l.name == tram.route_name)?.color,
+				},
+			};
+			geoJSON.features.push(feature);
+		}
+		return geoJSON;
+	};
+	// TODO: up until here ////
 
-  const [map, setMap] = useState<Map>(null);
+	const [map, setMap] = useState<Map>(null);
 
-  // GET THE GEOJSON
-  const stationGeoJSON = getStationData(stationData);
-  const lineGeoJSON = getLineData(lineData);
-  const tramGeoJSON = getTramData(tramData);
-  const view = new View({
-    center: OlProj.fromLonLat([8.5417, 47.3769]),
-    zoom: 15,
-  });
+	const view = new View({
+		center: OlProj.fromLonLat([8.5417, 47.3769]),
+		zoom: 15,
+	});
 
-  const stadiaLayer = new TileLayer({
-    source: new StadiaMaps({
-      layer: "alidade_smooth",
-      retina: true,
-    }),
-  });
+	const stadiaLayer = new TileLayer({
+		source: new StadiaMaps({
+			layer: "alidade_smooth",
+			retina: true,
+		}),
+	});
 
-  const osmLayer = new TileLayer({
-    source: new OSM(),
-  });
+	const osmLayer = new TileLayer({
+		source: new OSM(),
+	});
 
-  const lineLayer = new VectorLayer({
-    visible: true,
-    style: (feature) =>
-        new Style({
-          stroke: new Stroke({
-            width: 2,
-            color: feature.get("color"),
-          }),
-        }),
-  });
+	const lineLayer = new VectorLayer({
+		visible: true,
+		source: new VectorSource({
+			features: new GeoJSON().readFeatures(getLineData(lineData), {
+				featureProjection: view.getProjection(),
+			}),
+		}),
+		style: (feature) =>
+			new Style({
+				stroke: new Stroke({
+					width: 2,
+					color: feature.get("color"),
+				}),
+			}),
+	});
 
-  const stationLayer = new VectorLayer({
-    visible: true,
-    style: new Style({
-      image: new Circle({
-        radius: 5,
-        fill: new Fill({
-          color: "#000000",
-        }),
-      }),
-    }),
-  });
+	const stationLayer = new VectorLayer({
+		visible: true,
+		source: new VectorSource({
+			features: new GeoJSON().readFeatures(getStationData(stationData), {
+				featureProjection: view.getProjection(),
+			}),
+		}),
+		style: new Style({
+			image: new Circle({
+				radius: 5,
+				fill: new Fill({
+					color: "#000000",
+				}),
+			}),
+		}),
+	});
 
-  const tramLayer = new VectorLayer({
-    visible: true,
-    style: (feature) =>
-        new Style({
-          image: new Circle({
-            radius: 8,
-            fill: new Fill({
-              color: feature.get("color"),
-            }),
-          }),
-        }),
-  });
+	const tramLayer = new VectorLayer({
+		className: "trams",
+		visible: true,
+		source: new VectorSource({
+			features: new GeoJSON().readFeatures(getTramData(tramData), {
+				featureProjection: view.getProjection(),
+			})
+		}),
+		style: (feature) =>
+			new Style({
+				image: new Circle({
+					radius: 8,
+					fill: new Fill({
+						color: feature.get("color"),
+					}),
+				}),
+			}),
+	});
 
-  useEffect(() => {
-    const map = new Map({
-      target: "map",
-      layers: [osmLayer, lineLayer, stationLayer, tramLayer],
-    });
+	useEffect(() => {
+		map?.getAllLayers().find((v) => v.getClassName().startsWith("trams"))?.setSource(new VectorSource({
+			features: new GeoJSON().readFeatures(getTramData(tramData), {
+				featureProjection: view.getProjection(),
+			})
+		}))
+	}, [tramData]);
 
-    map.setView(
-        new View({
-          center: OlProj.fromLonLat([8.5417, 47.3769]),
-          zoom: 15,
-        })
-    );
+	useEffect(() => {
+		const map = new Map({
+			target: "map",
+			layers: [osmLayer, lineLayer, stationLayer, tramLayer],
+		});
 
-    map.on("click", function (e) {
-      map.forEachFeatureAtPixel(e.pixel, function (feature, layer) {
-        console.log(feature);
-      });
-    });
+		map.setView(
+			new View({
+				center: OlProj.fromLonLat([8.5417, 47.3769]),
+				zoom: 15,
+			})
+		);
 
-    setMap(map)
+		map.on("click", function (e) {
+			map.forEachFeatureAtPixel(e.pixel, function (feature, layer) {
+				console.log(feature);
+			});
+		});
 
-    return () => {
-      map.setTarget(null);
-      setMap(null);
-    };
-  }, []);
+		setMap(map)
 
-  useEffect(() => {
+		return () => {
+			map.setTarget(null);
+			setMap(null);
+		};
+	}, []);
 
-    lineLayer.setSource(new VectorSource({
-      features: new GeoJSON().readFeatures(lineGeoJSON, {
-        featureProjection: view.getProjection(),
-      }),
-    }))
-
-    stationLayer.setSource(new VectorSource({
-      features: new GeoJSON().readFeatures(stationGeoJSON, {
-        featureProjection: view.getProjection(),
-      }),
-    }))
-
-    tramLayer.setSource( new VectorSource({
-      features: new GeoJSON().readFeatures(tramGeoJSON, {
-        featureProjection: view.getProjection(),
-      }),
-    }))
-
-  }, []);
-
-  useEffect(() => {
-
-    // no idea if this works, couldn't test yet cuz after midnight all trams were suddenly gone
-    tramLayer.getSource().removeFeatures(tramLayer.getSource().getFeatures())
-	tramLayer.getSource().addFeatures(new GeoJSON().readFeatures(tramGeoJSON, {
-		featureProjection: view.getProjection(),
-	}))
-    tramLayer.getSource()?.dispatchEvent("change")
-
-  }, [tramData]);
-
-  return (
-      <>
-        <div id="map" style={{width: "100%", height: "100%"}}/>
-      </>
-  );
+	return (
+		<>
+			<div id="map" style={{width: "100%", height: "100%"}}/>
+		</>
+	);
 }
